6bfa591ffd3fb65ec5aaead1c7cbf68eb5b392d4
Bashar Shoumali <basharshomaly1@hotmail.com>
2025-09-16 12:29:53 +0300

    start to do the admins page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import UpdateProfile from "./pages/updateProfile/UpdateProfile";
-
+import Home from "./pages/home/Home";
+import AdminsPage from "./pages/admin/AdminsPage";
 export default function App() {
   return (
     <>
       <Navbar />
       <main>
         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin/home" element={<AdminsPage />} />
           <Route path="/account/update" element={<UpdateProfile />} />
         </Routes>
       </main>
---
+++ client/src/pages/admin/AdminsPage.jsx
@@ -0,0 +1,10 @@
+export default function AdminsPage() {
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-2">Welcome to E‑Tech</h1>
+      <p className="opacity-80">
+        Your one‑stop electronics shop. This is the Admin's page.
+      </p>
+    </section>
+  );
+}
